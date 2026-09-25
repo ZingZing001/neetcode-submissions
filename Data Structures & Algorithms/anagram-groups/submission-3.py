20fa55987b8a728f64a0ac30053aac140629097c
ZingZing_ <rzha972@aucklanduni.ac.nz>
2026-09-25 14:59:46 +1200

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # The common pattern will be that they will have the same letter if they are sorted
+        initDict = defaultdict(list)
+        for word in strs:
+            key = "".join(sorted(word))
+            initDict[key].append(word)
+        
+        return list(initDict.values())
